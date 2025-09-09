d66441d21162374d483027646c7098649cc77b53
ewebtechs <ewebtechs@gmail.com>
2025-09-09 02:03:35 +0100

    Use static generation for property pages

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({ properties }) {
   );
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const properties = await fetchProperties();
   return { props: { properties } };
 }
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,4 @@
-import { fetchPropertyById } from '../../lib/apex27';
+import { fetchPropertyById, fetchProperties } from '../../lib/apex27';
 
 export default function Property({ property }) {
   if (!property) return <div>Property not found</div>;
@@ -12,7 +12,15 @@ export default function Property({ property }) {
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  const properties = await fetchProperties();
+  return {
+    paths: properties.map((p) => ({ params: { id: p.id } })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
   const property = await fetchPropertyById(params.id);
   return { props: { property } };
 }
