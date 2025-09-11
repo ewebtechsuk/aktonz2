5405a7210920ebaa83adf7815159fc3e006cfe56
ewebtechs <ewebtechs@gmail.com>
2025-09-11 06:16:45 +0100

    Enable static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: { unoptimized: true },
   basePath: isProd && repo ? `/${repo}` : undefined,
   assetPrefix: isProd && repo ? `/${repo}/` : undefined,
