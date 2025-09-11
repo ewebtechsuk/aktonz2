79710b80cdd3c8f36a4d58d2d3a3daeaea90c2cb
ewebtechs <ewebtechs@gmail.com>
2025-09-11 02:36:34 +0100

    Add image slider to property cards

--- components/PropertyCard.js
+++ components/PropertyCard.js
@@ -1,10 +1,23 @@
+import ImageSlider from './ImageSlider';
+
 export default function PropertyCard({ property }) {
   const status = property.status ? property.status.replace(/_/g, ' ') : null;
   return (
     <div className="property-card">
       <div className="image-wrapper">
-        {property.image && (
-          <img src={property.image} alt={property.title} />
+        {property.images && property.images.length > 0 ? (
+          <ImageSlider
+            images={property.images}
+            imgStyle={{
+              width: '100%',
+              height: '200px',
+              objectFit: 'cover',
+              display: 'block',
+              border: 0,
+            }}
+          />
+        ) : (
+          property.image && <img src={property.image} alt={property.title} />
         )}
         {status && <span className="status-badge">{status}</span>}
       </div>
