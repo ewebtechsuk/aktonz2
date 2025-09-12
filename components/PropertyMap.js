2b4334b9f2cf400ac830b290b048f5d1d84995ec
ewebtechs <ewebtechs@gmail.com>
2025-09-12 23:32:38 +0100

    Add PropertyMap component

---
+++ components/PropertyMap.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Configure default icon paths so markers appear correctly
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
+export default function PropertyMap({ properties = [], center = [51.5, -0.1], zoom = 12 }) {
+  useEffect(() => {
+    const map = L.map('property-map').setView(center, zoom);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    properties.forEach((p) => {
+      if (typeof p.lat === 'number' && typeof p.lng === 'number') {
+        L.marker([p.lat, p.lng]).addTo(map).bindPopup(`<strong>${p.title}</strong>`);
+      }
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [properties, center, zoom]);
+
+  return <div id="property-map" style={{ height: '500px', width: '100%' }} />;
+}
