bd861cdacfd37a1e69f929df6081d17fe41acac3
ewebtechs <ewebtechs@gmail.com>
2025-09-13 04:32:26 +0100

    fix: reference Leaflet assets via CDN

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import '../styles/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/carousel.css';
+import '../styles/leaflet.css';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
