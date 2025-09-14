df30991b49a8fc9850346f88ba68cf688d33f3fc
ewebtechs <ewebtechs@gmail.com>
2025-09-15 00:13:53 +0100

    Refine header layout with Foxtons-style design

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,79 @@
+import { useState } from 'react';
 import Link from 'next/link';
-import { FaHome, FaKey, FaHeart, FaUser } from 'react-icons/fa';
 import styles from '../styles/Header.module.css';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+  const closeMenu = () => setMenuOpen(false);
+
+  const navLinks = (
+    <>
+      <Link href="/for-sale" className={styles.navLink} onClick={closeMenu}>
+        Buy
+      </Link>
+      <Link href="/to-rent" className={styles.navLink} onClick={closeMenu}>
+        Rent
+      </Link>
+      <Link href="/landlords" className={styles.navLink} onClick={closeMenu}>
+        Landlords
+      </Link>
+      <Link href="/sell" className={styles.navLink} onClick={closeMenu}>
+        Sell
+      </Link>
+      <Link href="/discover" className={styles.navLink} onClick={closeMenu}>
+        Discover
+      </Link>
+      <Link href="/contact" className={styles.navLink} onClick={closeMenu}>
+        Contact
+      </Link>
+    </>
+  );
+
+  const actionLinks = (
+    <>
+      <Link
+        href="/valuation"
+        className={styles.cta}
+        onClick={closeMenu}
+      >
+        Get a valuation
+      </Link>
+      <Link href="/login" className={styles.login} onClick={closeMenu}>
+        Login
+      </Link>
+    </>
+  );
+
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link href="/">Aktonz</Link>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/">Aktonz</Link>
+        </div>
 
+        <nav className={styles.nav}>{navLinks}</nav>
+
+        <div className={styles.actions}>
+          {actionLinks}
+          <button
+            className={styles.hamburger}
+            onClick={toggleMenu}
+            aria-label="Toggle menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
       </div>
-      <nav className={styles.nav}>
-        <Link href="/for-sale" className={styles.navLink}>
-          <FaHome className={styles.icon} />
-          <span className={styles.label}>Buy</span>
-        </Link>
-        <Link href="/to-rent" className={styles.navLink}>
-          <FaKey className={styles.icon} />
-          <span className={styles.label}>Rent</span>
-        </Link>
-        <Link href="/favourites" className={styles.navLink}>
-          <FaHeart className={styles.icon} />
-          <span className={styles.label}>Favourites</span>
-        </Link>
-        <Link href="/account" className={styles.navLink}>
-          <FaUser className={styles.icon} />
-          <span className={styles.label}>Account</span>
-        </Link>
+
+      <nav
+        className={`${styles.mobileMenu} ${menuOpen ? styles.menuOpen : ''}`}
+      >
+        {navLinks}
+        {actionLinks}
       </nav>
     </header>
   );
